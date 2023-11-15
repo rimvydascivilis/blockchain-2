353473657309b4c78a43a9b3cb822b520a9250a6
Rimvydas Civilis <rimvydascivilis@proton.me>
2023-11-15 16:42:34 +0200

    Use random func from random lib

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <random>
 
 #include "blockchain.h"
 #include "user.h"
@@ -10,32 +11,39 @@
 
 using namespace std;
 
+const uint USER_COUNT = 100;
+const uint TX_COUNT = 50;
+
 int main() {
-    srand(time(0));
+    random_device rd;
+    default_random_engine generator(rd());
+    uniform_int_distribution<uint64_t> balanceDistr(100000, 1000000);
+    uniform_int_distribution<uint64_t> userDistr(0, USER_COUNT - 1);
+    uniform_int_distribution<uint64_t> txDistr(1, 1000);
+
     Blockchain blockchain;
-
-    vector<User> users;
-    for (int i=0; i<1000; i++) {
-        string id = to_string(i + 1);
-        users.push_back(User("user" + id, rand() % 900001 + 100000, &blockchain));
-    }
     Miner miner = Miner("miner", &blockchain);
-    miner.mineBlock();
-    miner.mineBlock();
-    miner.mineBlock();
-    miner.mineBlock();
-    miner.mineBlock();
-    miner.mineBlock();
-    miner.mineBlock();
-    miner.mineBlock();
-    miner.mineBlock();
-
-    for (int i=0; i<10000; i++) {
-        User randomUser = users[rand() % users.size()];
-        User randomRecipient = users[rand() % users.size()];
-        randomUser.sendTokens(randomRecipient.getPublicKey(), rand() % 10);
+    vector<User> users;
+    for (uint i = 0; i < USER_COUNT; i++) {
+        string name = "user" + to_string(i);
+        token_t balance = balanceDistr(generator);
+        users.push_back(User(name, balance, &blockchain));
     }
+    miner.mineAllValidTxs();
 
-    miner.mineBlock();
+    for (uint i = 0; i < TX_COUNT; i++) {
+        uint64_t senderIndex = userDistr(generator);
+        uint64_t receiverIndex = userDistr(generator);
+        while (senderIndex == receiverIndex) {
+            receiverIndex = userDistr(generator);
+        }
+        token_t amount = txDistr(generator);
+        users[senderIndex].sendTokens(users[receiverIndex].getPublicKey(), amount);
+        if (i % 100 == 0) {
+            miner.mineAllValidTxs();
+        }
+    }
+    miner.mineAllValidTxs();
+
     writeToFile(blockchain, users, "results/");
 }
